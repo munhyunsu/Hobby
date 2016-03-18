7b1e60bb3af9a82f17e3dba69e4d3dda4c6ab73e
munhyunsu <munhyunsu@gmail.com>
2016-03-19 07:00:12 +0900

    주석표시한 과거 소스코드 삭제

--- alarm.py
+++ alarm.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-##### Needed Variable
-sleep_min = 240 # Sleep minute time
-start_vol = 0 # Start Volume (0.0, 1.0)
-end_vol = 1.0 # End Volume (0.0, 1.)
-duration_min = 30 # Going Time from start_vol to end_vol
-##### End Needed Variable
-
 import sys
 import dbus
 import time
@@ -72,10 +65,10 @@ def change_volume(prop_iface, start_vol, end_vol, duration_min):
 ##### Main Area
 def main():
 	music_player = 'clementine'
-	sleep_min = 1
+	sleep_min = 60 * 5
 	start_vol = 0.0
 	end_vol = 1.0
-	duration_min = 1
+	duration_min = 10
 
 	(prop_iface, player_iface) = connect_to_mpris2(music_player)
 
@@ -85,52 +78,10 @@ def main():
 
 	change_volume(prop_iface, start_vol, end_vol, duration_min)
 
-	change_volume(prop_iface, end_vol, start_vol, duration_min)
+#	change_volume(prop_iface, end_vol, start_vol, duration_min)
 	
-	stop_music(player_iface)
+#	stop_music(player_iface)
 ##### End of Main Area
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-#session_bus = dbus.SessionBus()
-#try:
-#	player = session_bus.get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
-#except dbus.exceptions.DBusException:
-#	print 'Before run this programm, You need to launch clementine'
-#	sys.exit()
-#print 'Start this programm'
-#
-#prop_iface = dbus.Interface(player, 'org.freedesktop.DBus.Properties')
-#player_iface = dbus.Interface(player, 'org.mpris.MediaPlayer2.Player')
-##prop_iface.Get('org.mpris.MediaPlayer2.Player', 'Volume')
-#
-#sleep_time = 60 * sleep_min
-#
-#vol_level = (end_vol - start_vol) / duration_min
-#
-#print 'I sleep for ' + str(sleep_time) + ' Seconds'
-#time.sleep(sleep_time)
-#
-#print 'Play the Music'
-#prop_iface.Set('org.mpris.MediaPlayer2.Player', 'Volume', 0)
-#player_iface.Play()
-#
-#print 'Set volume to ' + str(start_vol)
-#cur_vol = start_vol
-#prop_iface.Set('org.mpris.MediaPlayer2.Player', 'Volume', start_vol)
-#for vol_step in range(1, duration_min+1):
-#	time.sleep(60)
-#	cur_vol = cur_vol + vol_level
-#	print 'Set volume(' + str(vol_step) + 'step) to ' + str(cur_vol)
-#	prop_iface.Set('org.mpris.MediaPlayer2.Player', 'Volume', cur_vol)
-#print 'Set volume(last) to ' + str(cur_vol)
-#prop_iface.Set('org.mpris.MediaPlayer2.Player', 'Volume', end_vol)
-#
-#print 'End this programm'
-
-
