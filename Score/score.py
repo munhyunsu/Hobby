b1de0038d7f1c5a7161bcc11917802e22c329f6f
munhyunsu <munhyunsu@gmail.com>
2016-05-30 22:21:34 +0900

    입력 출력 완성

--- Score/score.py
+++ Score/score.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+##### 임포트 시작 #####
+# 인자 얻기
+import sys
+# csv 다루기
+import csv
 
+##### 임포트 끝 #####
 
 
 
 ##### 메인 시작 #####
 def main():
-	print 'Wow'
+	if( len(sys.argv) < 3 ):
+		print '사용법: python score.py 입력파일 출력파일'
+		sys.exit(0)
+
+	print '받은 입력:', str(sys.argv[0]), str(sys.argv[1]), str(sys.argv[2])
+	print str(sys.argv[1]), '에 있는 답안 결과를', str(sys.argv[2]), \
+		'와 대조해 점수를 출력합니다.'
+	
 ##### 메인 끝 #####
 
 
