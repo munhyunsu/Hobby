ad5c1a6cbeef583716f3bd031e45d66200f2c40c
munhyunsu <munhyunsu@gmail.com>
2016-12-27 15:50:10 +0900

    프로젝트 분리

--- IPTVAnalysis/example_execute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 igmpreport_extract_from_pcap.py target.pcap igmpreport.json 
-python3 channel_analysis_from_igmpreport.py igmpreport.json channel.json
-python3 remove_unknown_channel.py channel.json viewchannel.json
-python3 program_analysis.py viewchannel.json program.json
-python3 iptv_diary.py program.json iptvdiary.json
