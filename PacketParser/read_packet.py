4faddaa1a9f094656b91ae1ff9f3662c5f307fad
munhyunsu <munhyunsu@gmail.com>
2018-01-17 13:31:24 -0500

    packet pasing

--- PacketParser/read_packet.py
+++ PacketParser/read_packet.py
@@ -8,15 +8,17 @@ def main(argv):
     print(pcap.findalldevs())
     read_interface = create_reading_interface()
 
-    print(dir(read_interface))
-    while True:
+    #while True:
+    for index in range(0, 1):
         print(read_interface.stats(), read_interface.readpkts())
-#        print(read_interface.dispatch())
+        #print(read_interface.readpkts())
+
 
 
 
 def create_reading_interface():
-    read_interface = pcap.pcap(name = None, 
+    #read_interface = pcap.pcap(name = None, 
+    read_interface = pcap.pcap(name = 'wlp4s0', 
                                promisc = False,
                                timeout_ms = 100)
     return read_interface
