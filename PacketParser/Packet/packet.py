c0535d68c2a19b89526d776220650d984f693df9
munhyunsu <munhyunsu@gmail.com>
2018-01-18 09:52:41 -0500

    init

---
+++ PacketParser/Packet/packet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+L2UNKNOWN = 0x0000
+L2IP = 0x0800
+L3UNKNOWN = 0x00
+L3TCP = 0x06
+L3UDP = 0x11
--- PacketParser/read_packet.py
+++ PacketParser/read_packet.py
@@ -9,18 +9,20 @@ def main(argv):
     read_interface = create_reading_interface()
 
     #while True:
-    for index in range(0, 1):
-        print(read_interface.stats(), read_interface.readpkts())
+    for index in range(0, 10):
+        #print(read_interface.stats(), read_interface.readpkts())
         #print(read_interface.readpkts())
+        for timestamp, read_data in read_interface.readpkts():
+            print(timestamp, read_data)
 
 
 
 
 def create_reading_interface():
     #read_interface = pcap.pcap(name = None, 
-    read_interface = pcap.pcap(name = 'wlp4s0', 
+    read_interface = pcap.pcap(name = 'wlp4s0',
                                promisc = False,
-                               timeout_ms = 100)
+                               timeout_ms = 1000)
     return read_interface
 
 
