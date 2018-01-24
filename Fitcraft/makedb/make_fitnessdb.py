8df34b727bfb58f91ee8d508cd25b8719b99c666
munhyunsu <munhyunsu@gmail.com>
2018-01-24 13:54:35 -0500

    make db

--- Fitcraft/makedb/make_fitnessdb.py
+++ Fitcraft/makedb/make_fitnessdb.py
@@ -9,7 +9,7 @@ import sqlite3
 FORMAT = '%(created)s:%(levelno)s:%(message)s'
 logging.basicConfig(stream = sys.stdout,
                     format = FORMAT,
-                    level = logging.DEBUG)
+                    level = logging.INFO)
 
 def main(argv):
     if len(argv) < 2:
@@ -21,37 +21,65 @@ def main(argv):
         if os.path.isdir(arg) == True:
             dir_queue.append(arg)
 
-    (connector, cursor) = connect_database('fitcraft.db')
+    connector = connect_database('fitcraft.db')
 
     for file_path in get_file_list(dir_queue):
-        print(get_data(file_path))
         # we can access all of data
+        insert_data(connector, get_data(file_path))
         break
 
     close_database(connector)
 
 
 
-def insert_data(connector, cursor, data):
+def insert_data(connector, data):
     # TODO(LuHa): data parsing, insert
-    pass
+    if 'activities-steps' in data:
+        db_name = 'steps'
+        date = data['activities-steps'][0]['dateTime']
+        for values in data['activities-steps-intraday']['dataset']:
+            logging.debug(
+                    (db_name, date, values['time'], values['value']))
+
+    if 'sleep' in data:
+        db_name = 'sleeps'
+        date = data['sleep'][0]['dateOfSleep']
+        for values in data['sleep'][0]['minuteData']:
+            logging.debug(
+                    (db_name, date, values['dateTime'], values['value']))
+    if 'activities-heart' in data:
+        db_name = 'hearts'
+        date = data['activities-heart'][0]['dateTime']
+        for values in data['activities-heart-intraday']['dataset']:
+            logging.debug(
+                    (db_name, date, values['time'], values['value']))
 
 
-def create_tables(connector, cursor):
+
+def create_tables(connector):
+    cursor = connector.cursor()
     # it contain ROWID INTEGER PRIMARY KEY defaults
     cursor.execute('''CREATE TABLE IF NOT EXISTS steps (
-                        datetime TEXT NOT NULL,
+                        datetime INTEGER NOT NULL,
                         user TEXT NOT NULL,
                         value INTEGER NOT NULL)
-                   '''
+                   ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS hearts (
+                        datetime INTEGER NOT NULL,
+                        user TEXT NOT NULL,
+                        value INTEGER NOT NULL)
+                   ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS sleeps (
+                        datetime INTEGER NOT NULL,
+                        user TEXT NOT NULL,
+                        value INTEGER NOT NULL)
+                   ''')
     connector.commit()
 
 
 def connect_database(name = 'fitcraft.db'):
     connector = sqlite3.connect(name)
-    cursor = connector.cursor()
-    
-    return (connector, cursor)
+    return connector
 
 
 
