481ec658e708c2c4df18ad13073a923069567a1e
munhyunsu <munhyunsu@gmail.com>
2018-03-02 12:23:33 +0900

    script

--- HTMLGame/C2/js/game.js
+++ HTMLGame/C2/js/game.js
@@ -14,3 +14,38 @@ var game={
 $(window).load(function() {
   game.init*(;
 });
+
+var levels={
+  // Level data
+  data:[
+    {  // First level
+      foreground:'desert-foreground',
+      background:'clouds-background',
+      entities:[]
+    },
+    {  // Second level
+      foreground:'desert-foreground',
+      background:'clouds-background',
+      entities[]
+    }
+  ],
+  // Initialize level selection screen
+  init:function(){
+    var html="";
+    for (var i=0; i<levels.data.length; i++) {
+      var level=levels.data[i];
+      html+= '<input type="button" value="'+(i+1)+'">';
+    };
+    $('#levelselectscreen').html(html);
+
+    // Set the button click event handlers to load level
+    $('#levelselectscreen input').click(function(){
+      levels.load(this.value-1);
+      $('#levelselectscreen').hide();
+    });
+  },
+
+  // Load all data and images for a specific level
+  load:function(number){
+  }
+}
