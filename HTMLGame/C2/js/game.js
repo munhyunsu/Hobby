14327bef6399bff821d8c1342b9c776073d17355
munhyunsu <munhyunsu@gmail.com>
2018-03-04 05:56:03 +0900

    init

--- HTMLGame/C2/js/game.js
+++ HTMLGame/C2/js/game.js
@@ -49,3 +49,16 @@ var levels={
   load:function(number){
   }
 }
+
+init: function(){
+  // Initialize objects
+  levels.init();
+
+  // Hide all game layers and display the start screen
+  $('.gamelayer').hide();
+  $('#gamestartscreen').show();
+
+  // Get handler for game canvas and context
+  game.canvas=$('#gamecanvas')[0];
+  game.context=game.canvas.getContext('2d');
+}, 
