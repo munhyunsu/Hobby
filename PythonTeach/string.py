1ebdcfbc539d185b38ef420841e19e7b1dea51fa
munhyunsu <munhyunsu@gmail.com>
2018-04-08 08:50:38 +0900

    string example

---
+++ PythonTeach/string.py
@@ -0,0 +1,5 @@
+string = input('영어로 아무거나 입력하세요(5자 이상): ')
+
+print('str.upper(): {0}'.format(string.upper()))
+print('str.lower(): {0}'.format(string.lower()))
+print('str[2:4]: {0}'.format(string[2:4]))
