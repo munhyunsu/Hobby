ce906a2fe0badbce199046155bf90d2f97c532a0
munhyunsu <munhyunsu@gmail.com>
2018-04-08 08:11:20 +0900

    while

---
+++ PythonTeach/while_example.py
@@ -0,0 +1,8 @@
+inputted_var = int(input('0을 입력하면 종료합니다: '))
+
+while inputted_var != 0:
+    print('{0}이 입력되었습니다.'.format(inputted_var))
+    inputted_var = int(input('0을 입력하면 종료합니다: '))
+
+print('{0}이 입력되었습니다. 종료합니다.'.format(inputted_var))
+
