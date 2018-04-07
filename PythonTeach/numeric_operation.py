d83cc991296f1415dd61b30632481f1a8fb5be8b
munhyunsu <munhyunsu@gmail.com>
2018-04-08 08:44:03 +0900

    numeric

---
+++ PythonTeach/numeric_operation.py
@@ -0,0 +1,24 @@
+num1 = float(input('첫번째 수: '))
+num2 = float(input('두번째 수: '))
+
+result1 = num1 + num2
+print('x + y: {0}'.format(result1))
+
+result2 = num1 - num2
+print('x - y: {0}'.format(result2))
+
+result3 = num1 * num2
+print('x * y: {0}'.format(result3))
+
+result4 = num1 - num2
+print('x / y: {0}'.format(result4))
+
+result5 = num1 - num2
+print('x // y: {0}'.format(result5))
+
+result6 = num1 % num2
+print('x % y: {0}'.format(result6))
+
+result7 = num1 ** num2
+print('x ** y: {0}'.format(result7))
+
