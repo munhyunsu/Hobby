446f69ee970beb7080b46a2a5f4b37607fd5ff3d
munhyunsu <munhyunsu@gmail.com>
2018-04-09 14:12:52 +0900

    modify PT

--- PythonTeach/if.py
+++ PythonTeach/if.py
@@ -1,7 +1,7 @@
 user_age = int(input('나이: '))
 
 if user_age > 15:
-    print('당신의 나이는 15살보다 많군요.\n')
+    print('당신의 나이는 15살보다 많군요.')
 else:
-    print('당신의 나이는 15살보다 적군요.\n')
+    print('당신의 나이는 10살보다 적군요.')
 
--- PythonTeach/int_float.py
+++ PythonTeach/int_float.py
@@ -1,8 +1,11 @@
 num = input('정수 혹은 소수를 입력하세요: ')
+print(type(num))
 
 float_num = float(num)
+print(type(float_num))
 print('입력한 숫자의 소수형: {0}'.format(float_num))
 
 int_num = int(float_num)
+print(type(int_num))
 print('입력한 숫자의 정수형: {0}'.format(int_num))
 
