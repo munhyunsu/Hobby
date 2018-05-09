3bd33a0d9fb537dad195d9cae4ab8676b1bb2fb6
munhyunsu <munhyunsu@gmail.com>
2018-05-09 10:21:21 +0900

    tcp echoserver c

--- SocketExample/tcp_server.c
+++ SocketExample/tcp_server.c
@@ -2,10 +2,15 @@
   Socket example: server
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+// http://man7.org/linux/man-pages/man2/listen.2.html
+#define MAXBACKLOG 5
+#define BUFFSIZE 1500
 
 int main(int argc, char* argv[]) {
     /* prepare local variables */
@@ -13,6 +18,8 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in addr_server, addr_client; // Addresses
     unsigned short port_server; // Port number of server
     unsigned int client_addr_len; // client address structure
+    int readlen; // readed buf length
+    char buf[BUFFSIZE]; // read buffer
 
     /* arguments parsing */
     if(argc != 2) { // check server port as arguments
@@ -28,10 +35,10 @@ int main(int argc, char* argv[]) {
     }
 
     /* server address structing */
-    memset(&addr_server, 0, sizeof(addr_server)) // zero set
-    sddr_server.sin_family = AF_INET; // Internet address family
+    memset(&addr_server, 0, sizeof(addr_server)); // zero set
+    addr_server.sin_family = AF_INET; // Internet address family
     addr_server.sin_addr.s_addr = htonl(INADDR_ANY); // any interface
-    addr_server.sin_port = htons(port_server) // port number
+    addr_server.sin_port = htons(port_server); // port number
 
     /* bind socket to address and port */
     if( bind(sock_server, (struct sockaddr *) &addr_server, 
@@ -41,9 +48,40 @@ int main(int argc, char* argv[]) {
     }
 
     /* listen connection */
-    
+    if( listen(sock_server, MAXBACKLOG) < 0) {
+        printf("listen() error\n");
+        exit(1);
+    }
 
+    /* client accept */
+    while(1) {
+        /* create client socket */
+        client_addr_len = sizeof(addr_client);
+        if( (sock_client = accept(sock_server, 
+                                  (struct sockaddr *) &addr_client,
+                                  &client_addr_len)) < 0) {
+            printf("accept() error\n");
+            exit(1);
+        }
 
+        /* serve client */
+        printf("Connected %s\n", inet_ntoa(addr_client.sin_addr));
+        while(1) {
+            if( (readlen = recv(sock_client, buf, BUFFSIZE, 0)) < 0) {
+                printf("recv() error\n");
+                exit(1);
+            }
+            if(readlen > 0) {
+                if( send(sock_client, buf, readlen, 0) != readlen ) {
+                    printf("send() error\n");
+                    exit(1);
+                }
+            } else if(readlen == 0) {
+                close(sock_client);
+                break;
+            }
+        }       
+    }
 
     return 0;
 }
