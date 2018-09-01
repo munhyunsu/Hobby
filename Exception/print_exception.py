912d609a8cd30142eadfa7946f7de817c20b3236
munhyunsu <munhyunsu@gmail.com>
2018-09-01 12:42:18 +0900

    trace string

--- Exception/print_exception.py
+++ Exception/print_exception.py
@@ -10,8 +10,9 @@ def main():
     try:
         raise_exception()
     except Exception as e:
-        traceback.print_exc()
-
+        #traceback.print_exc()
+        trace_string = traceback.format_exc()
+        print(trace_string)
 
 if __name__ == '__main__':
     main()
