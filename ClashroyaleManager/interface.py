f2e744ad289a076d51222f4dee5db75320655c30
munhyunsu <munhyunsu@gmail.com>
2019-03-29 19:40:13 +0900

    interface intro

--- ClashroyaleManager/interface.py
+++ ClashroyaleManager/interface.py
@@ -1,8 +1,15 @@
 from print_util import pprint
 
+
 class CLI(object):
     @staticmethod
     def intro():
         pprint('===== Clash Royale Clan Member Manager =====')
+        pprint('|1. View current member                    |')
+        pprint('|2. Input member who attended clan war day |')
+        pprint('|3. Show filtered member                   |')
+        pprint('============================================')
+
 
 cli = CLI()
+
