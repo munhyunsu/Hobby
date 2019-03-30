b2caac1c140a6567355b0ff6cefb0cbe9104a306
munhyunsu <munhyunsu@gmail.com>
2019-03-30 10:39:29 +0900

    pretty input

--- ClashroyaleManager/interface.py
+++ ClashroyaleManager/interface.py
@@ -4,12 +4,14 @@ from print_util import pprint
 class CLI(object):
     @staticmethod
     def intro():
+        intro_key = {'1': 'view_current_member',
+                     '2': 'input_clan_war_member',
+                     '3': 'show_filtered_member'}
         pprint('===== Clash Royale Clan Member Manager =====')
         pprint('|1. View current member                    |')
         pprint('|2. Input member who attended clan war day |')
         pprint('|3. Show filtered member                   |')
         pprint('============================================')
-
 
 cli = CLI()
 
--- ClashroyaleManager/print_util.py
+++ ClashroyaleManager/print_util.py
@@ -4,3 +4,11 @@ def pprint(data, style=0):
     if style == 0:
         print('\x1B[33m\x1B[100m{0}\x1B[0m'.format(data))
 
+def pinput(data, style=0):
+    """
+    """
+    if style == 0:
+        value = input('\x1B[33m\x1B[100m{0}\x1B[0m'.format(data))
+
+    return value
+        
