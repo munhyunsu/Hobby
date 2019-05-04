41243dff66a347708ef49bb88932fe5021c16941
munhyunsu <munhyunsu@gmail.com>
2019-05-04 13:40:50 +0900

    Q1 tests

---
+++ CharRemoved/test_char_removed.py
@@ -0,0 +1,27 @@
+import unittest
+
+from char_removed import equalsWhenOneCharRemoved
+
+
+class CharRemovedTests(unittest.TestCase):
+    def test_case1(self):
+        self.assertFalse(equalsWhenOneCharRemoved('x', 'y'))
+
+    def test_case2(self):
+        self.assertFalse(equalsWhenOneCharRemoved('x', 'XX'))
+
+    def test_case3(self):
+        self.assertFalse(equalsWhenOneCharRemoved('yy', 'yx'))
+
+    def test_case4(self):
+        self.assertTrue(equalsWhenOneCharRemoved('abcd', 'abxcd'))
+
+    def test_case5(self):
+        self.assertTrue(equalsWhenOneCharRemoved('xyz', 'xz'))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
