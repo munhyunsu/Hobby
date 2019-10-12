18366d24d85f70f0d5daba0bee85e3170fe0d962
munhyunsu <munhyunsu@gmail.com>
2019-10-13 06:00:02 +0900

    tutorial

--- TensorflowTutorials/FederatedLearning/tutorial.py
+++ TensorflowTutorials/FederatedLearning/tutorial.py
@@ -27,12 +27,15 @@ if six.PY3:
   tff.framework.set_default_executor(
       tff.framework.create_local_executor(NUM_CLIENTS))
 
+print('If tff was installed successfully, then it print out "Hello, World!"')
 print(tff.federated_computation(lambda: 'Hello, World!')())
 
 
 emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
+print('The length of emnist_traint.client_ids: ')
 print(len(emnist_train.client_ids))
 
+print('and The types and shapes for output: ')
 print(emnist_train.output_types, emnist_train.output_shapes)
 
 example_dataset = emnist_train.create_tf_dataset_for_client(
@@ -40,11 +43,75 @@ example_dataset = emnist_train.create_tf_dataset_for_client(
 
 example_element = iter(example_dataset).next()
 
+print('Print out the "label": ')
 print(example_element['label'].numpy())
 
-
+print('and Show image for example_element: ')
 from matplotlib import pyplot as plt
 plt.imshow(example_element['pixels'].numpy(), cmap='gray', aspect='equal')
 plt.grid('off')
-_ = plt.show()
+#_ = plt.show()
 
+
+## Define preprocessing function
+NUM_CLIENTS = 10
+NUM_EPOCHS = 10
+BATCH_SIZE = 20
+SHUFFLE_BUFFER = 500
+
+def preprocess(dataset):
+
+  def element_fn(element):
+    return collections.OrderedDict([
+        ('x', tf.reshape(element['pixels'], [-1])),
+        ('y', tf.reshape(element['label'], [1])),
+    ])
+
+  return dataset.repeat(NUM_EPOCHS).map(element_fn).shuffle(
+      SHUFFLE_BUFFER).batch(BATCH_SIZE)
+
+
+preprocessed_example_dataset = preprocess(example_dataset)
+
+sample_batch = tf.nest.map_structure(
+    lambda x: x.numpy(), iter(preprocessed_example_dataset).next())
+
+print('Check the sample batch dataset: ')
+print(sample_batch)
+
+
+## Make subset for federated data
+def make_federated_data(client_data, client_ids):
+  return [preprocess(client_data.create_tf_dataset_for_client(x))
+          for x in client_ids]
+
+sample_clients = emnist_train.client_ids[0:NUM_CLIENTS]
+
+federated_train_data = make_federated_data(emnist_train, sample_clients)
+
+print('The length of federated dataset and sample: ')
+print(len(federated_train_data), federated_train_data[0])
+
+
+## create federated model
+def create_compiled_keras_model():
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Dense(
+          10, activation=tf.nn.softmax, kernel_initializer='zeros', input_shape=(784,))])
+
+  model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      optimizer=tf.keras.optimizers.SGD(learning_rate=0.02),
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+  return model
+
+
+## create model function
+def model_fn():
+  keras_model = create_compiled_keras_model()
+  return tff.learning.from_compiled_keras_model(keras_model, sample_batch)
+
+iterative_process = tff.learning.build_federated_averaging_process(model_fn)
+
+print('The federated process: ')
+print(str(iterative_process.initialize.type_signature))
