61582bd7769cb46b665f1a07cf4683cf61965ab9
munhyunsu <munhyunsu@gmail.com>
2019-12-29 11:09:14 +0900

    sha256

--- FileNumbering/file_obj.py
+++ FileNumbering/file_obj.py
@@ -1,8 +1,27 @@
+import hashlib
+import shutil
+
+BUF_SIZE = 64*1024
+
+
 class File(object):
     def __init__(self, src):
         self.path = src
-        self.obj = open(src, 'r')
+        self.sha256 = self._get_sha256()
     
+    def _get_sha256(self):
+        sha256 = hashlib.sha256()
+        
+        with open(self.path, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha256.update(data)
+
+        return sha256.hexdigest()
+
     def move(self, dst):
         # Check sha256
-        pass
+        shutil.move(self.path, dst)
+
