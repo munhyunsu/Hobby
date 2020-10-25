5ec0c11bc89161221528d33b9579444ba8418ecd
munhyunsu <munhyunsu@gmail.com>
2020-10-25 11:21:03 +0900

    DF to HTML table

--- ExamScore/start.py
+++ ExamScore/start.py
@@ -1,9 +1,8 @@
 import os
-import csv
+import pandas as pd
 
 from flask import Flask, request, render_template, redirect
 
-RESULT = os.path.abspath(os.path.expanduser('./midresult.csv'))
 
 def stoi(l):
     result = list()
@@ -18,41 +17,32 @@ app = Flask(__name__)
 data = dict()
 
 
-with open(RESULT, 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        num = row[0]
-        name = row[1]
-        s1 = stoi(row[2:12])
-        s2 = stoi(row[12:18])
-        s3 = stoi(row[18:21])
-        s4 = stoi(row[21:25])
-        s5 = stoi(row[25:29])
-        print(f'Loaded: {num} {name}')
-        data[(num, name)] = (s1, s2, s3, s4, s5)
-
+df = pd.read_csv('score.csv')
+df = df.fillna(0)
+df[df.columns.values[3:]] = df[df.columns.values[3:]].astype('int32')
+#print(df)
 
 @app.route('/', methods=['GET'])
 def score():
     num = request.args.get('num')
     if num is not None:
-        num = num.strip()
-    name = request.args.get('name')
-    if name is not None:
-        name = name.strip()
-    score = data.get((num, name), None)
-    if score is not None:
-        tscore = list()
-        for s in score:
-            tscore.append(sum(s))
-        total = sum(tscore)
-        print(f'{num} {name} {score}')
-        return render_template('index.html', num=num,
-                                             name=name,
-                                             score=score,
-                                             tscore=tscore,
-                                             total=total,
-                                             length=len(score))
+        num = int(num.strip())
+    phone = request.args.get('phone')
+    if phone is not None:
+        phone = phone.strip()
+    try:
+        if (df[df['학번'] == num]['번호'] == phone).values[0]:
+            ss = df[df['학번'] == num]
+        else:
+            ss = None
+    except:
+        ss = None
+        pass
+    if ss is not None:
+        print(f'{ss}')
+        return render_template('index.html', 
+                               tables=[ss.to_html(index=False,
+                                                  justify='center')],)
     else:
         return render_template('index.html')
 
