025109d77f94e89acbd74e7d1b721caf97214e96
munhyunsu <munhyunsu@gmail.com>
2022-03-09 20:27:40 +0900

    Button and text find in main activity

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MainActivity.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MainActivity.java
@@ -3,12 +3,22 @@ package org.cs_cnu.morsecode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText box_text;
+    private Button btn_speaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        box_text = (EditText) findViewById(R.id.box_text);
+        btn_speaker = (Button) findViewById(R.id.btn_speaker);
+
     }
 }
