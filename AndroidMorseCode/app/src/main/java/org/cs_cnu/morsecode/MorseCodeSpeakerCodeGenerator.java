ca36f044ba0586de7fceed3cd7938a4041b5ea14
munhyunsu <munhyunsu@gmail.com>
2022-03-13 06:18:32 +0900

    StringBuilder to create morse code

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseCodeSpeakerCodeGenerator.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseCodeSpeakerCodeGenerator.java
@@ -8,10 +8,16 @@ public class MorseCodeSpeakerCodeGenerator {
 
     final String message;
     final Map<String, String> map;
+    final String morse_code;
 
     public MorseCodeSpeakerCodeGenerator(String message, Map<String, String> map) {
         this.message = message;
         this.map = map;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("-.-. ... .");
+
+        this.morse_code = sb.toString();
         Log.i("MorseCode", "Created");
     }
 }
