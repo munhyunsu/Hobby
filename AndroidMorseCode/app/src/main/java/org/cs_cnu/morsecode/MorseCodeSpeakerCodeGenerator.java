04ceeb40ab6bffebf6cfbc7e9d81d295d31a64f5
munhyunsu <munhyunsu@gmail.com>
2022-03-12 09:14:35 +0900

    Code Generator

---
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseCodeSpeakerCodeGenerator.java
@@ -0,0 +1,9 @@
+package org.cs_cnu.morsecode;
+
+import android.util.Log;
+
+public class MorseCodeSpeakerCodeGenerator {
+    public MorseCodeSpeakerCodeGenerator() {
+        Log.i("MorseCode", "Created");
+    }
+}
