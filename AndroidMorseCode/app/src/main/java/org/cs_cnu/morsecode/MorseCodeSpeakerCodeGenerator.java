617724abbd03019006cd41914ba1ca8e8323d864
munhyunsu <munhyunsu@gmail.com>
2022-03-12 22:04:47 +0900

    Morse code encoder

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseCodeSpeakerCodeGenerator.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseCodeSpeakerCodeGenerator.java
@@ -2,8 +2,16 @@ package org.cs_cnu.morsecode;
 
 import android.util.Log;
 
+import java.util.Map;
+
 public class MorseCodeSpeakerCodeGenerator {
-    public MorseCodeSpeakerCodeGenerator() {
+
+    final String message;
+    final Map<String, String> map;
+
+    public MorseCodeSpeakerCodeGenerator(String message, Map<String, String> map) {
+        this.message = message;
+        this.map = map;
         Log.i("MorseCode", "Created");
     }
 }
