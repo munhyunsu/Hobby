66676578922c412b106f9694de55be51969eb273
munhyunsu <munhyunsu@gmail.com>
2022-03-18 08:42:11 +0900

    Thread parameter for normalized processing

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseSpeakerThread.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseSpeakerThread.java
@@ -2,11 +2,16 @@ package org.cs_cnu.morsecode;
 
 public class MorseSpeakerThread extends Thread {
 
+    public interface MorseSpeakerCallback {
+        void onProgress(int current, int total);
+        void onDone();
+    }
+
     public interface MorseSpeakerIterator extends Iterable<String> {
         int getSize();
     }
 
-    public MorseSpeakerThread() {
+    public MorseSpeakerThread(MorseSpeakerIterator iterator, MorseSpeakerCallback callback) {
         setPriority(Thread.MAX_PRIORITY);
     }
 }
