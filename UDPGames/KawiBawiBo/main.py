611b87fb6b483afac19686e8aa9f2f8d96b6b861
munhyunsu <munhyunsu@gmail.com>
2022-04-09 21:46:37 +0900

    Argument config

--- UDPGames/KawiBawiBo/main.py
+++ UDPGames/KawiBawiBo/main.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true',
                         help='The present debug message')
+    parser.add_argument('--address', type=str, default='localhost',
+                        help='The address to serve service')
+    parser.add_argument('--port', type=int, default=6292+2022,
+                        help='The poort to serve service')
 
     FLAGS, _ = parser.parse_known_args()
     DEBUG = FLAGS.debug
