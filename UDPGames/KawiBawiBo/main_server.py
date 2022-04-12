a2463152d7b2b4b283e941a0632c753aec5b5195
munhyunsu <munhyunsu@gmail.com>
2022-04-12 10:42:11 +0900

    Game server

--- UDPGames/KawiBawiBo/main_server.py
+++ UDPGames/KawiBawiBo/main_server.py
@@ -4,13 +4,48 @@ import socketserver
 FLAGS = _ = None
 DEBUG = False
 
+lock = threading.Lock()
+storage = []
+
+
+class Player():
+    def __init__(self, name, hands):
+        self.name = name
+        self.hands = hands
+
+    def __repr__(self):
+        return f'{self.name} {self.hands}'
+
+
+def process_message(message):
+    tokens = message.split(' ')
+    name = tokens[0]
+    hands = []
+    for token in tokens[1:]:
+        token = token.lower()
+        if token not in ('kawi', 'bawi', 'bo'):
+            raise Exception
+        hands.append(token)
+    player = Player(name, hands)
+    return player
+
 
 class ThreadedUDPRequestHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         message = self.rfile.read().decode('utf-8')
-        print(f'{message} from {self.client_address}')
-        data = message.encode('utf-8')
-        self.wfile.write(data)
+        if DEBUG:
+            print(f'{message} from {self.client_address}')
+        lock.acquire()
+        try:
+            card = process_message(message)
+            storage.append(process_message(message))
+            data = f'Ok'.encode('utf-8')
+            self.wfile.write(data)
+        except:
+            data = f'Error'.encode('utf-8')
+            self.wfile.write(data)
+        finally:
+            lock.release()
 
 
 class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
@@ -22,7 +57,6 @@ def main():
         print(f'Parsed arguments {FLAGS}')
         print(f'Unparsed arguments {_}')
 
-    
     server = ThreadedUDPServer((FLAGS.address, FLAGS.port),
                                ThreadedUDPRequestHandler)
     server.allow_reuse_address = True
@@ -34,6 +68,9 @@ def main():
             server_thread.join()
         except KeyboardInterrupt:
             server.shutdown()
+
+    for item in storage:
+        print(item)
 
 
 if __name__ == '__main__':
