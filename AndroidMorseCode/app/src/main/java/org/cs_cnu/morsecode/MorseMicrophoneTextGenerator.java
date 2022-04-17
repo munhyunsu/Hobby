4e61bbaa1273b07e4685111350338fe3e3295483
munhyunsu <munhyunsu@gmail.com>
2022-04-17 09:05:22 +0900

    Microphone generator

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseMicrophoneTextGenerator.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseMicrophoneTextGenerator.java
@@ -6,9 +6,16 @@ public class MorseMicrophoneTextGenerator {
 
     final String morse_code;
     final Map<String, String> map;
+    final String text;
 
     public MorseMicrophoneTextGenerator(String morse_code, Map<String, String> map) {
         this.morse_code = morse_code;
         this.map = map;
+
+        this.text = "";
+    }
+
+    public String getText() {
+        return this.text;
     }
 }
