2c8b6e6410a27f07b58f945d32d0dd4eb5d14609
munhyunsu <munhyunsu@gmail.com>
2022-04-19 10:08:21 +0900

    Thread creator

--- AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseMicrophoneThread.java
+++ AndroidMorseCode/app/src/main/java/org/cs_cnu/morsecode/MorseMicrophoneThread.java
@@ -1,4 +1,16 @@
 package org.cs_cnu.morsecode;
 
 public class MorseMicrophoneThread extends Thread {
+    public interface MorseMicrophoneCallback {
+        void onProgress(String value);
+        void onDone(String value);
+    }
+
+    public MorseMicrophoneThread(MorseMicrophoneThread.MorseMicrophoneCallback callback,
+                                 int sample_rate, float frequency, float unit) {
+    }
+
+    @Override
+    public void run() {
+    }
 }
