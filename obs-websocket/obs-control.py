e219da86e5c6f37560b76cf6f9be0b00e964829c
munhyunsu <munhyunsu@gmail.com>
2023-02-12 20:52:18 +0900

    Web socket creation

--- obs-websocket/obs-control.py
+++ obs-websocket/obs-control.py
@@ -1,7 +1,27 @@
+import os
+
+import simpleobsws
+
+import secret
+
+FLAGS = _ = None
+DEBUG = False
+
 
 def main():
-    pass
+    if DEBUG:
+        print(f'Parsed arguments {FLAGS}')
+        print(f'Unparsed arguments {_}')
 
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true',
+                        help='The present debug message')
+
+    FLAGS, _ = parser.parse_known_args()
+    DEBUG = FLAGS.debug
+
     main()
