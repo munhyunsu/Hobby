f41c8616b006179f6e60258211cddd2208131cd9
munhyunsu <munhyunsu@gmail.com>
2023-02-15 04:42:08 +0900

    OBS Start and Stop record with websocket

--- obs-websocket/obs-control.py
+++ obs-websocket/obs-control.py
@@ -9,34 +9,6 @@ import secret
 FLAGS = _ = None
 DEBUG = False
 
-async def make_job(ws, job):
-    await ws.connect()
-    await ws.wait_until_identified()
-
-    request = simpleobsws.Request(job)
-
-    ret = await ws.call(request)
-    if ret.ok():
-        pprint.pprint(ret.responseData)
-
-    await ws.disconnect()
-
-
-def main():
-    if DEBUG:
-        print(f'Parsed arguments {FLAGS}')
-        print(f'Unparsed arguments {_}')
-
-    ws = simpleobsws.WebSocketClient(url=f'ws://{secret.ip}:{secret.port}',
-                                     password=f'{secret.password}')
-
-    #asyncio.run(make_job(ws, FLAGS.job))
-    #loop = asyncio.new_event_loop()
-    #asyncio.set_event_loop(loop)
-    #loop = asyncio.get_event_loop()
-    #loop.run_until_complete(make_job(ws, FLAGS.job))
-    asyncio.create_task(make_job(ws, FLAGS.job))
-
 
 async def amain():
     if DEBUG:
@@ -56,9 +28,6 @@ async def amain():
         pprint.pprint(ret.responseData)
 
     await ws.disconnect()
-
-    #await asyncio.create_task(make_job(ws, FLAGS.job))
-
 
 
 if __name__ == '__main__':
