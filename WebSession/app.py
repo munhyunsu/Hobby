b601a6b882ed74e99b8a24e5a238370750fab177
munhyunsu <munhyunsu@gmail.com>
2023-03-01 09:52:33 +0900

    Index run

--- WebSession/app.py
+++ WebSession/app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, session, render_template
 
 app = Flask(__name__)
 app.secret_key = 'It is my secret key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
