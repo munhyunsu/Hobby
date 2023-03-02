9c443f33fc6111e0f4d2e1ed4afbf4ef1b81f9ac
munhyunsu <munhyunsu@gmail.com>
2023-03-02 10:03:37 +0900

    username in session

--- WebSession/app.py
+++ WebSession/app.py
@@ -1,8 +1,19 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'It is my secret key'
 
 @app.route('/')
 def index():
+    print(session)
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        print(session)
+        return redirect(url_for('index'))
+    print(session)
+    return render_template('login.html')
