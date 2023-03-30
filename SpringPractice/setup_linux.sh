e997f728367695ba937ddc155c06275450d85626
munhyunsu <munhyunsu@gmail.com>
2023-03-30 20:04:55 +0900

    Spring setup

---
+++ SpringPractice/setup_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://openjdk.org/
+## https://openjdk.org/projects/jdk/20/
+wget https://download.java.net/java/GA/jdk20.0.1/b4887098932d415489976708ad6d1a4b/9/GPL/openjdk-20.0.1_linux-x64_bin.tar.gz -O openjdk-20.0.1_linux-x64_bin.tar.gz
+tar -xvf openjdk-20.0.1_linux-x64_bin.tar.gz
+
+# https://gradle.org/
+## https://gradle.org/releases/
+wget https://services.gradle.org/distributions/gradle-8.1-all.zip -O gradle-8.1-all.zip
+unzip gradle-8.1-all.zip
+
