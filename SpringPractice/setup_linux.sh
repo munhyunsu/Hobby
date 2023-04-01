7b94420f9fad9757fe216814b3ab345082bdae80
munhyunsu <munhyunsu@gmail.com>
2023-04-01 09:14:52 +0900

    Bellsoft JDK

--- SpringPractice/setup_linux.sh
+++ SpringPractice/setup_linux.sh
@@ -2,8 +2,12 @@
 
 # https://openjdk.org/
 ## https://openjdk.org/projects/jdk/20/
-wget https://download.java.net/java/GA/jdk20.0.1/b4887098932d415489976708ad6d1a4b/9/GPL/openjdk-20.0.1_linux-x64_bin.tar.gz -O openjdk-20.0.1_linux-x64_bin.tar.gz
-tar -xvf openjdk-20.0.1_linux-x64_bin.tar.gz
+#wget https://download.java.net/java/GA/jdk20.0.1/b4887098932d415489976708ad6d1a4b/9/GPL/openjdk-20.0.1_linux-x64_bin.tar.gz -O openjdk-20.0.1_linux-x64_bin.tar.gz
+#tar -xvf openjdk-20.0.1_linux-x64_bin.tar.gz
+
+# https://bell-sw.com/
+## https://bell-sw.com/pages/downloads/
+wget https://download.bell-sw.com/java/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz -O bellsoft-jdk17.0.7+7-linux-amd64.tar.gz
 
 # https://gradle.org/
 ## https://gradle.org/releases/
