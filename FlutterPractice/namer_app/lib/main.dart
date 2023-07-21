ad4b2f30fe5628dadb8dea4593bb22e1bea4378a
munhyunsu <munhyunsu@gmail.com>
2023-07-22 08:14:49 +0900

    getNext word listeners

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
