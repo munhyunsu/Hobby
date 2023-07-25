821bd863f9d738e692d443263c939d96cc09eafd
munhyunsu <munhyunsu@gmail.com>
2023-07-25 21:19:54 +0900

    Button scheme

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -69,9 +69,12 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(20.0),
         child: Text(pair.asLowerCase),
       ),
     );
