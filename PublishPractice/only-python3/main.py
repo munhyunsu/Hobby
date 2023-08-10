d8ef4a67bc65890c1a1ce468624e58755f6aa859
munhyunsu <munhyunsu@gmail.com>
2023-08-11 07:52:11 +0900

    HTTP server using python3

--- PublishPractice/only-python3/main.py
+++ PublishPractice/only-python3/main.py
@@ -14,6 +14,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true',
                         help='The present debug message')
+    parser.add_argument('--host', default='0.0.0.0', type=str,
+                        help='The serving IP address')
+    parser.add_argument('--port', default=8888, type=int,
+                        help='The serving port number')
+    parser.add_argument('--prefix', default='', type=str,
+                        help='The URL prefix')
+    parser.add_argument('--data', default='./data', type=str,
+                        help='The web data directory')
 
     FLAGS, _ = parser.parse_known_args()
     DEBUG = FLAGS.debug
