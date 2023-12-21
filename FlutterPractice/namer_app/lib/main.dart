db5ca41e712a0af6d9db17a6400da5483d481de8
munhyunsu <munhyunsu@gmail.com>
2023-12-21 21:35:39 +0900

    Nav rail

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -82,6 +82,49 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class BigCard extends StatelessWidget {
   const BigCard ({
