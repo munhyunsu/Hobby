1623b61b27154c82ce83f56a9ac0bb767d9c5185
munhyunsu <munhyunsu@gmail.com>
2023-12-22 08:14:54 +0900

    State app

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -45,7 +45,13 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() =>
+  _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
