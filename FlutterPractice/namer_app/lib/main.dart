c89c10c0c29c497842fc89cde6b3f3838c0c1630
munhyunsu <munhyunsu@gmail.com>
2023-12-23 22:14:55 +0900

    placeholder

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
