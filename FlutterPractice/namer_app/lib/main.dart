ff951382628f94a8f2d20d2f96761b595f2225d3
munhyunsu <munhyunsu@gmail.com>
2023-12-25 08:11:36 +0900

    Change placeholder to Favorates page

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
