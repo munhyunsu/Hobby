be2f8f2fdcbc920cf4b65fe1485b5f5323697889
munhyunsu <munhyunsu@gmail.com>
2023-12-24 20:39:58 +0900

    Favorites page

--- FlutterPractice/namer_app/lib/main.dart
+++ FlutterPractice/namer_app/lib/main.dart
@@ -178,3 +178,31 @@ class BigCard extends StatelessWidget {
     );
   }
 }
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+            '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
