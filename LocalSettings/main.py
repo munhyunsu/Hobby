8199dd1a80105767ec22624b95844e9f4dd01e89
munhyunsu <munhyunsu@gmail.com>
2024-03-17 08:14:21 +0900

    test for inheritated conf

---
+++ LocalSettings/main.py
@@ -0,0 +1,17 @@
+import conf
+from conf import *
+
+VAR1 = 'VAR1 of main.py'
+
+
+
+def main():
+    print(__file__, VAR1)
+    print(__file__, VAR2)
+    print(__file__, conf.VAR1)
+    print(__file__, conf.VAR2)
+
+
+if __name__ == '__main__':
+    main()
+
