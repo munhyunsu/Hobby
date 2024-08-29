e5eed0724ab3e4234e91176fea4aca86dc58d39a
munhyunsu <munhyunsu@gmail.com>
2024-08-29 21:41:25 +0900

    mkdir nodejs

--- Environments/download_nodejs.sh
+++ Environments/download_nodejs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
-tar -xvf node-v20.17.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz -O nodejs.tar.xz
+mkdir -p nodejs
+tar -xvf node-v20.17.0-linux-x64.tar.xz -C nodejs
 
