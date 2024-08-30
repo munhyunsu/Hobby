67af50707c5dee493fc0d98f1272da9d9cedd2db
munhyunsu <munhyunsu@gmail.com>
2024-08-30 11:12:42 +0900

    Remove nodejs

--- Environments/download_nodejs.sh
+++ Environments/download_nodejs.sh
@@ -2,5 +2,6 @@
 
 wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz -O nodejs.tar.xz
 mkdir -p nodejs
-tar -xvf node-v20.17.0-linux-x64.tar.xz -C nodejs
+tar -xvf node-v20.17.0-linux-x64.tar.xz -C nodejs --strip-components 1
+rm nodejs.tar.xz
 
