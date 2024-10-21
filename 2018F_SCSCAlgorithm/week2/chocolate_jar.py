76e2647811b88d6070948c6c8f4591b6c2aa908b
munhyunsu <munhyunsu@gmail.com>
2024-10-21 20:42:44 +0900

    remove 2018F_SCSCAlgorithm/week2/chocolate_jar.py

--- 2018F_SCSCAlgorithm/week2/chocolate_jar.py
+++
@@ -1,7 +0,0 @@
-class ChocolateJar(object):
-    def __init__(self, chocolates=13):
-        self.chocolates = chocolates+1
-
-    def take_chocolate(self, hands):
-        self.chocolates = self.chocolates - hands
-
