0e45e990c2fe4f2896a451fca3c0f4d45903ce87
munhyunsu <munhyunsu@gmail.com>
2025-01-21 10:34:40 +0900

    Download NodeJS in local

--- Environments/Node.js/download_nodejs.sh
+++ Environments/Node.js/download_nodejs.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz -O nodejs.tar.xz
+NODE_VERSION="v22.14.0"
+BASE_URL="https://nodejs.org/dist/$NODE_VERSION"
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "$OS" == "Linux" ]]; then
+    NODE_TAR="node-$NODE_VERSION-linux-x64.tar.xz"
+elif [[ "$OS" == "Darwin" ]]; then
+    if [[ "$ARCH" == "arm64" ]]; then
+        NODE_TAR="node-$NODE_VERSION-darwin-arm64.tar.gz"
+    else
+        NODE_TAR="node-$NODE_VERSION-darwin-x64.tar.gz"
+    fi
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+NODE_URL="$BASE_URL/$NODE_TAR"
+
+curl -# -C - -o $NODE_TAR $NODE_URL
 mkdir -p nodejs
-tar -xvf node-v20.17.0-linux-x64.tar.xz -C nodejs --strip-components 1
-rm nodejs.tar.xz
+tar -xvf $NODE_TAR -C nodejs --strip-components 1
+rm $NODE_TAR
 
