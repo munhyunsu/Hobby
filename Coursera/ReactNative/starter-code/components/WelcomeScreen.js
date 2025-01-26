dbbfa19069765e81b00b7010173ff1918b066202
munhyunsu <munhyunsu@gmail.com>
2025-01-26 21:30:25 +0900

    fontsize for answer

--- Coursera/ReactNative/starter-code/components/WelcomeScreen.js
+++ Coursera/ReactNative/starter-code/components/WelcomeScreen.js
@@ -9,6 +9,7 @@ export default function WelcomeScreen() {
     }}>
       <Text
         style={{
+          fontSize: 18,
           textAlign: 'center',
       }}>
         Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
