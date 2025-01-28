47a0e4928c22324a3f1c7491ef7e4fe67bf06914
munhyunsu <munhyunsu@gmail.com>
2025-01-28 20:14:37 +0900

    ScrollView answer

--- Coursera/ReactNative/scrollview-starter-code/WelcomeScreen.js
+++ Coursera/ReactNative/scrollview-starter-code/WelcomeScreen.js
@@ -3,35 +3,33 @@ import { View, Text, ScrollView } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <ScrollView
-        indicatorStyle={'white'}
-        horizontal={false}
-        style={{
-          backgroudColor: 'black',
+    <ScrollView
+      indicatorStyle={'white'}
+      horizontal={false}
+      style={{
+        flex: 1,
       }}>
-        <Text
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 50,
+          color: '#EDEFEE',
+          textAlign: 'center',
+        }}>
+        Welcome to Little Lemon
+      </Text>
+      <Text
           style={{
-            padding: 40,
-            fontSize: 50,
-            color: '#EDEFEE',
-            textAlign: 'center',
-          }}>
-          Welcome to Little Lemon
-        </Text>
-        <Text
-          style={{
-            fontSize: 38,
-            padding: 20,
-            marginVertical: 8,
-            color: '#EDEFEE',
-            textAlign: 'center',
-          }}>
-          Little Lemon is a charming neighborhood bistro that serves simple food
-          and classic cocktails in a lively but casual environment. We would love
-          to hear more about your experience with us!
-        </Text>
-      </ScrollView>
-    </View>
+          fontSize: 38,
+          padding: 20,
+          marginVertical: 8,
+          color: '#EDEFEE',
+          textAlign: 'center',
+        }}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
+      </Text>
+    </ScrollView>
   );
 }
