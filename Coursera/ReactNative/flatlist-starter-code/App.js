92cc94d79a682eecdbae35f31a05e306051c867d
munhyunsu <munhyunsu@gmail.com>
2025-01-31 08:44:35 +0900

    Flatlist working

--- Coursera/ReactNative/flatlist-starter-code/App.js
+++ Coursera/ReactNative/flatlist-starter-code/App.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <View style={styles.container}>
         <LittleLemonHeader />
         <WelcomeScreen />
+        <MenuItems />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
---
+++ Coursera/ReactNative/flatlist-starter-code/components/MenuItems.js
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+
+const menuItemsToDisplay = [
+  { name: 'Hummus', id: '1A' },
+  { name: 'Moutabal', id: '2B' },
+  { name: 'Falafel', id: '3C' },
+  { name: 'Marinated Olives', id: '4D' },
+  { name: 'Kofta', id: '5E' },
+  { name: 'Eggplant Salad', id: '6F' },
+  { name: 'Lentil Burger', id: '7G' },
+  { name: 'Smoked Salmon', id: '8H' },
+  { name: 'Kofta Burger', id: '9I' },
+  { name: 'Turkish Kebab', id: '10J' },
+  { name: 'Fries', id: '11K' },
+  { name: 'Buttered Rice', id: '12L' },
+  { name: 'Bread Sticks', id: '13M' },
+  { name: 'Pita Pocket', id: '14N' },
+  { name: 'Lentil Soup', id: '15O' },
+  { name: 'Greek Salad', id: '16Q' },
+  { name: 'Rice Pilaf', id: '17R' },
+  { name: 'Baklava', id: '18S' },
+  { name: 'Tartufo', id: '19T' },
+  { name: 'Tartufo', id: '20U' },
+  { name: 'Tiramisu', id: '21V' },
+  { name: 'Panna Cotta', id: '22W' },
+];
+
+export default function MenuItems() {
+  const Item = ({ name }) => (
+    <View style={styles.innerContainer}>
+      <Text style={styles.itemText}>
+        {name}
+      </Text>
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item name={item.name} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>
+        View Menu
+      </Text>
+      <FlatList
+        data={menuItemsToDisplay}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.75,
+  },
+  innerContainer: {
+    backgroundColor: 'black',
+  },
+  headerText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  itemText: {
+    color: '#F4CE14',
+  }
+});
