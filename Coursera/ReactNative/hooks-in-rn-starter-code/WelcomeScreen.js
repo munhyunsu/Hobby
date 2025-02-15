88b3b92ffcb49f92f488cdc734c2f5317815c903
munhyunsu <munhyunsu@gmail.com>
2025-02-16 06:04:25 +0900

    useColorScheme

--- Coursera/ReactNative/hooks-in-rn-starter-code/WelcomeScreen.js
+++ Coursera/ReactNative/hooks-in-rn-starter-code/WelcomeScreen.js
@@ -1,8 +1,10 @@
-import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image, useColorScheme } from 'react-native';
 
 export default function WelcomeScreen() {
+  const colorScheme = useColorScheme();
+
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={[styles.container, colorScheme === 'light' ? {backgroundColor: '#EDEFEE'} : {backgroundColor: '#333333'}]}>
       <View style={styles.headerWrapper}>
         <Image
           style={styles.image}
@@ -11,13 +13,15 @@ export default function WelcomeScreen() {
           accessible={true}
           accessibilityLabel={'Little Lemon Logo'}
         />
-
-        <Text style={styles.headerText}>Little Lemon</Text>
+        <Text style={[styles.headerText, colorScheme === 'light' ? {color: '#333333'} : {color: '#EDEFEE'}]}>Little Lemon</Text>
       </View>
-      <Text style={styles.regularText}>
+      <Text style={[styles.regularText, colorScheme === 'light' ? {color: '#333333'} : {color: '#EDEFEE'}]}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear your experience with us!
+      </Text>
+      <Text style={[styles.regularText, colorScheme === 'light' ? {color: '#333333'} : {color: '#EDEFEE'}]}>
+        Color Scheme: {colorScheme}
       </Text>
     </ScrollView>
   );
