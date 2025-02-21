5ccf2224284dbc7c219415c5adcec13e5164d9ba
munhyunsu <munhyunsu@gmail.com>
2025-02-22 08:37:20 +0900

    Navigator answer

--- Coursera/ReactNative/setup-routes-starter-code/App.js
+++ Coursera/ReactNative/setup-routes-starter-code/App.js
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
 import LoginScreen from './LoginScreen';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+
   return (
-    <>
-        <View style={styles.container}>
-          <LittleLemonHeader />
-        </View>
-        <View style={styles.footerContainer}>
-          <LittleLemonFooter />
-        </View>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
