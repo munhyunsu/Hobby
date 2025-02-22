fb6554469953336667efe24ba1deabee7df94ffd
munhyunsu <munhyunsu@gmail.com>
2025-02-22 21:18:55 +0900

    Navigator answer with InitialRouteName

--- Coursera/ReactNative/setup-routes-starter-code/App.js
+++ Coursera/ReactNative/setup-routes-starter-code/App.js
@@ -14,7 +14,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Welcome" component={WelcomeScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
       </Stack.Navigator>
