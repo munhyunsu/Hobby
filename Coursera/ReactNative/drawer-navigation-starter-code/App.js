9497db5baa1c6bfa904938c0b67a2fdb2f6f5b48
munhyunsu <munhyunsu@gmail.com>
2025-03-04 01:58:40 +0900

    Drawer Navigation Answer

--- Coursera/ReactNative/drawer-navigation-starter-code/App.js
+++ Coursera/ReactNative/drawer-navigation-starter-code/App.js
@@ -6,9 +6,9 @@ import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
 import LoginScreen from './LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
@@ -16,10 +16,10 @@ export default function App() {
       <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
+          <Drawer.Navigator useLegacyImplementation>
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+          </Drawer.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
--- Coursera/ReactNative/drawer-navigation-starter-code/babel.config.js
+++ Coursera/ReactNative/drawer-navigation-starter-code/babel.config.js
@@ -1,6 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset'
+    ],
+    plugins: [
+      '@babel/plugin-transform-export-namespace-from',
+      'react-native-reanimated/plugin'
+    ]
   };
 };
+
+
