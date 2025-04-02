614a2c1c8176320196e3f551b4e35f1014393bf3
munhyunsu <munhyunsu@gmail.com>
2025-04-02 19:14:33 +0900

    User manager router

--- LoginToken/backend/app/user_manager/user_manager.py
+++ LoginToken/backend/app/user_manager/user_manager.py
@@ -4,3 +4,18 @@ from . import database, models
 
 
 models.Base.metadata.create_all(bind=database.engine)
+
+router = APIRouter(
+    prefix='/user-manager',
+    tags=['User manager'],
+    responses={404: {'description': 'Not Found'}},
+)
+
+
+@router.get('/info', include_in_schema=False)
+async def get_info():
+    return {'prefix': router.prefix,
+            'tags': router.tags,
+            'responses': router.responses}
+
+
