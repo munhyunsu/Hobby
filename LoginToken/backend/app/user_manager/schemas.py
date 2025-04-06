c2aae1bea8d8ed9ea64bf2c033d9026320d8e80d
munhyunsu <munhyunsu@gmail.com>
2025-04-06 09:18:45 +0900

    Login schema

--- LoginToken/backend/app/user_manager/schemas.py
+++ LoginToken/backend/app/user_manager/schemas.py
@@ -1,17 +1,19 @@
 import re
-
 from pydantic import BaseModel, EmailStr, validator, Field
 
 
-class UserBase(BaseModel):
+class UsernameValidatorMixin(BaseModel):
     username: str = Field(min_length=3, max_length=20)
-    email: EmailStr
 
     @validator('username')
     def validate_username(cls, v):
         if not re.match(r'^[a-zA-Z0-9_]+$', v):
             raise ValueError('username must contain only letters and numbers (no spaces or special characters)')
         return v
+
+
+class UserBase(UsernameValidatorMixin):
+    email: EmailStr
 
 
 class UserCreate(UserBase):
@@ -23,6 +25,10 @@ class UserCreate(UserBase):
         if 'password' in values and v != values['password']:
             raise ValueError('password does not match with confirm_password')
         return v
+
+
+class UserLogin(UsernameValidatorMixin):
+    password: str = Field(min_length=6)
 
 
 class User(UserBase):
