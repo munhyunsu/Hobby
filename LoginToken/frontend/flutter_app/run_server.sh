4f7df1ba47526064d5ec315cc4dafe935acc0596
munhyunsu <munhyunsu@gmail.com>
2025-04-10 20:16:56 +0900

    Flutter run with dart-define

--- LoginToken/frontend/flutter_app/run_server.sh
+++ LoginToken/frontend/flutter_app/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-flutter --device-id web-server run --web-hostname 127.0.0.1 --web-port 8080
+flutter --device-id web-server run --web-hostname 127.0.0.1 --web-port 8080 --dart-define-from-file config.json
