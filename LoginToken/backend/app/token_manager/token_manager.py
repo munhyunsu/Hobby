496890b65874240faa3a6d1bcf6863ce24898ff6
munhyunsu <munhyunsu@gmail.com>
2025-04-13 09:05:53 +0900

    Authenticate user endpoint

--- LoginToken/backend/app/token_manager/token_manager.py
+++ LoginToken/backend/app/token_manager/token_manager.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, status as HTTPStatus
+from sqlalchemy.orm import Session
 
+from .. import database
+from ..user_manager import schemas as user_schemas, utils as user_utils
+from . import schemas
 
 router = APIRouter(
     prefix='/token-manager',
@@ -13,4 +17,18 @@ async def get_info():
     return {'prefix': router.prefix,
             'tags': router.tags,
             'responses': router.responses}
+
+
+@router.post('/token', response_model=schemas.Token)
+async def post_token(
+    user: user_schemas.UserLogin,
+    db: Session = Depends(database.get_db),
+):
+    username = user.username.lower()
+    password = user.password
+    db_user = utils.authenticate_user(db=db, username=username, password=password)
+    if not db_user:
+        raise HTTPException(status_code=HTTPStatus.HTTP_403_FORBIDDEN,
+                            detail='Forbidden request')
+    return db_user
 
