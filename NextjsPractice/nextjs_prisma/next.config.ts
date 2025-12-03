c2bc9c54160c0fe8f3fc927c1863c93ab6176380
munhyunsu <munhyunsu@gmail.com>
2025-12-03 14:12:05 +0900

    Ready

--- NextjsPractice/nextjs_prisma/next.config.ts
+++ NextjsPractice/nextjs_prisma/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
